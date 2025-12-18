b000b83cfeef0b861d58852b60dd5c6f14916b32
AstridLin <astridlin@kkbox.com>
2025-12-18 09:51:42 +0800

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Astrid Lin on 2025/12/18.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlayerTests/VideoPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerTests.swift
+//  VideoPlayerTests
+//
+//  Created by Astrid Lin on 2025/12/18.
+//
+
+import Testing
+@testable import VideoPlayer
+
+struct VideoPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
